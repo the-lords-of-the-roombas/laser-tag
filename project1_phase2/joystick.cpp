00bbc0c784ce9629f85f3813f2e3437519ae73a1
Jakob Leben <jakob.leben@gmail.com>
2015-02-02 16:31:47 -0800

    pr1-ph2: adjust joystick mapping

--- project1_phase2/joystick.cpp
+++ project1_phase2/joystick.cpp
@@ -17,14 +17,14 @@ void joystick_read(void* data)
 
   int x = analogRead(x_pin);
   int y = analogRead(y_pin);
+  bool sw = digitalRead(sw_pin);
+
 
   // FIXME: joystick original range may be smaller:
-  x = map(x, 0, 1024, -100, 100);
-  y = map(y, 0, 1024, -100, 100);
-
-  bool sw = !digitalRead(sw_pin);
+  x = map(x, 0, 350, -100, 100);
+  y = map(y, 0, 350, -100, 100);
 
   s->x = x;
   s->y = y;
-  s->sw = sw;
+  s->sw = !sw;
 }
