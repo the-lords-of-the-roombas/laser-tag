d220346fd98ef3737d283c3ca419aede3074a424
Jakob Leben <jakob.leben@gmail.com>
2015-03-07 03:38:44 -0700

    arduino upload script: code file = command line argument

--- project2/upload.sh
+++ project2/upload.sh
@@ -1,5 +1,12 @@
+code_file=$1
+
+if [[ -z $code_file ]]; then
+  echo "Missing argument: file to upload"
+  exit 1
+fi
+
 /usr/share/arduino/hardware/tools/avrdude \
 -C/usr/share/arduino/hardware/tools/avrdude.conf \
 -v -v -v -v -patmega2560 -cwiring -P/dev/ttyACM0 \
 -b115200 -D -V \
--Uflash:w:rtos/test/test.hex:i
+-Uflash:w:$code_file:i
