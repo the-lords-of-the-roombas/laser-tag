8fb8768eb432bab887e0e22ee3733127996dabf7
Jakob Leben <jakob.leben@gmail.com>
2015-03-21 13:13:11 -0600

    P3: global radio packets API

---
+++ project3/radio_packets.hpp
@@ -0,0 +1,28 @@
+#ifndef RADIO_PACKETS_INCLUDED
+#define RADIO_PACKETS_INCLUDED
+
+#include "radio_packets_global.hpp"
+
+#include <stdint.h>
+
+typedef struct radio_packet
+{
+    uint8_t type;
+    uint16_t timestamp;
+
+    // Payload
+    union
+    {
+        // "_filler" just makes sure the packet is exactly 32 bytes long.
+        uint8_t _filler[29];
+
+        // global packets
+        game_status_payload game_status;
+        shot_payload shot;
+
+        // your other packets...
+    };
+
+} radio_packet_t;
+
+#endif // RADIO_PACKETS_INCLUDED
---
+++ project3/radio_packets_global.hpp
@@ -0,0 +1,25 @@
+#ifndef GLOBAL_RADIO_PACKETS_INCLUDED
+#define GLOBAL_RADIO_PACKETS_INCLUDED
+
+enum global_packet_types
+{
+    game_status_packet_type = 0,
+    shot_packet_type,
+
+    // ... there may be more in future ...
+
+    global_packet_type_count = 20
+};
+
+struct game_status_payload
+{
+    uint8_t it_id;
+};
+
+struct shot_payload
+{
+    uint8_t target_id;
+    uint8_t shooter_id;
+};
+
+#endif // GLOBAL_RADIO_PACKETS_INCLUDED
