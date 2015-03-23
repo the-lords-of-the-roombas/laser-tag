f859369da8e63a2d01c2f38672081fc470da5642
Jakob Leben <jakob.leben@gmail.com>
2015-03-23 16:09:56 -0600

    P3: start the "gun" class

---
+++ project3/gun.cpp
@@ -0,0 +1,19 @@
+#include "gun.hpp"
+
+namespace robot_tag_game {
+
+gun::gun()
+{
+}
+
+void gun::init()
+{
+
+}
+
+void gun::run()
+{
+
+}
+
+}
---
+++ project3/gun.hpp
@@ -0,0 +1,16 @@
+#ifndef ROBOT_TAG_GUN_INCLUDED
+#define ROBOT_TAG_GUN_INCLUDED
+
+namespace robot_tag_game {
+
+class gun
+{
+public:
+    gun();
+    void init();
+    void run();
+};
+
+}
+
+#endif // ROBOT_TAG_GUN_INCLUDED
