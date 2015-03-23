07bb9e821c4ca4f2cd4c2620a1642f548158537b
Jakob Leben <jakob.leben@gmail.com>
2015-03-23 13:33:11 -0600

    P3: arduino upload script: no need for verbose output

--- project3/arduino-upload.sh
+++ project3/arduino-upload.sh
@@ -7,6 +7,6 @@ fi
 
 /usr/share/arduino/hardware/tools/avrdude \
 -C/usr/share/arduino/hardware/tools/avrdude.conf \
--v -v -v -v -patmega2560 -cwiring -P/dev/ttyACM0 \
+-patmega2560 -cwiring -P/dev/ttyACM0 \
 -b115200 -D -V \
 -Uflash:w:$code_file:i
