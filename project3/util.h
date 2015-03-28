f7be2f9603ee59d5e31218c4525d2c4b41ba9727
Jakob Leben <jakob.leben@gmail.com>
2015-03-27 20:42:48 -0600

    P3: util: add bytes_to_int16

--- project3/util.h
+++ project3/util.h
@@ -1,7 +1,22 @@
 #ifndef RTOS_UTIL_INCLUDED
 #define RTOS_UTIL_INCLUDED
 
+#include <stdint.h>
+
 #define SET_BIT(FIELD, BIT) (FIELD |= _BV(BIT))
 #define CLEAR_BIT(FIELD, BIT) (FIELD &= ~_BV(BIT))
+
+namespace robot_tag_game {
+
+inline int16_t bytes_to_int16(uint8_t h, uint8_t l)
+{
+    uint16_t result = 0;
+    result |= h & 0xFF;
+    result <<= 8;
+    result |= l & 0xFF;
+    return result;
+}
+
+}
 
 #endif // RTOS_UTIL_INCLUDED
