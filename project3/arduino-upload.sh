d332001979326a587a264105913802c7b4f52260
Jakob Leben <jakob.leben@gmail.com>
2015-04-08 06:52:32 -0700

    arduino upload script: add port argument

--- project3/arduino-upload.sh
+++ project3/arduino-upload.sh
@@ -1,12 +1,17 @@
 code_file=$1
+port=$2
 
 if [[ -z $code_file ]]; then
   echo "Missing argument: file to upload"
   exit 1
 fi
 
+if [[ -z $port ]]; then
+  port=/dev/ttyACM0
+fi
+
 /usr/share/arduino/hardware/tools/avrdude \
 -C/usr/share/arduino/hardware/tools/avrdude.conf \
--patmega2560 -cwiring -P/dev/ttyACM0 \
+-patmega2560 -cwiring -P$port \
 -b115200 -D -V \
 -Uflash:w:$code_file:i
